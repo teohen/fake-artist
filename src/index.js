eb37b6166b67569f2200998698513bf68e263f0c
teohen <teodorohenriquecarvalho@gmail.com>
2020-11-26 21:58:11 -0300

    feat(routes): added more routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import  App  from "./App"
+import App from "./App"
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
 import CreateGame from './pages/create-game.page';
 import Game from './pages/game.page';
+import CreatePlayer from './pages/create-player.page';
+import GameMenu from './pages/game-menu.page';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Switch>
-      <Route path="/"  exact={true} component={App} />
-      <Route path="/create-game"  exact={true} component={CreateGame} />
-      <Route path="/game/:code"  exact={true} component={Game} />
+      <Switch>
+        <Route path="/" exact={true} component={App} />
+        <Route path="/create-game" exact={true} component={CreateGame} />
+        <Route path="/create-player" exact={true} component={CreatePlayer} />
+        <Route path="/game/:code" exact={true} component={Game} />
+        <Route path="/menu" exact={true} component={GameMenu} />
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
